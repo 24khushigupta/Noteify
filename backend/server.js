63289d2506040744e3700e1f8eafcfb12536e453
unknown <khushugupta2001@.com>
2026-07-12 00:55:37 +0530

    Fix API URL

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ dotenv.config();
 const app =express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://noteify-smoky-two.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 console.log("Registering auth routes...");
 app.use("/api/auth", authRoutes);
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: " https://noteify-bvnp.onrender.com",
+  baseURL: "https://noteify-bvnp.onrender.com/api",
 });
 
 export default API;
