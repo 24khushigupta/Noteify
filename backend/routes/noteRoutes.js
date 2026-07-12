8729d2ec38d342033035ff464a508511b84c6489
unknown <khushugupta2001@.com>
2026-07-12 17:28:24 +0530

    Fix Cloudinary storage configuration

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer");
 
 const Note = require("../models/Note");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
 const cloudinary = require("../config/cloudinary");
 
 const router = express.Router();
